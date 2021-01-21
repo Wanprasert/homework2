d04b620d5aeb5cdee648566e590a42a57a9a9a42
Wanprasert <77339479+Wanprasert@users.noreply.github.com>
2021-01-21 21:18:21 +0700

    Add files via upload

---
+++ hw2_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string name;
+	int salary, sale, cp, tcp, total;
+	cout << " *********Homework 2*********" <<endl;
+	cout << " Enter Name : ";
+	cin >>name;
+	cout << " Enter Salary : ";
+	cin >>salary;
+	cout << " Enter Sale : ";
+	cin >>sale;
+	cout << " Enter Commission Percent: ";
+	cin >>cp;
+	tcp = cp*sale/100;
+	total = tcp+salary;
+	cout << " ----------output----------" <<endl;
+	cout << " Your Name = "<<name <<endl;
+	cout << " Total Revenue : "<<total <<" Baht" <<endl;
+	cout << " ---------------------------" <<endl;
+}
